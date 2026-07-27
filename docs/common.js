1d93da9aa093c8cd143a4d3192bc0d04226605ab
louiscklaw <louiscklaw@users.noreply.github.com>
2026-07-27 19:51:17 +0800

    docs: ensure return to home navigation supports both local development and GitHub Pages production URLs

--- docs/common.js
+++ docs/common.js
@@ -1,4 +1,11 @@
 
 function backToHome() {
-    window.location.href = "/";
+    console.log(window.location.href);
+    if (window.location.href.search('127') > -1) {
+        window.location.href = "/";
+    } else {
+        // https://louiscklaw.github.io/YT091DS/
+        window.location.href = "https://louiscklaw.github.io/YT091DS/";
+    }
+
 }
